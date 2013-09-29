da6daa96937b5e22012484828b01ac8f2b16b344
rawdom <nopenothappening@live.com.au>
2013-09-29 16:20:18 +1000

    gsrg

--- common/net/deathware/tut/t10/Arrays.java
+++ common/net/deathware/tut/t10/Arrays.java
@@ -3,7 +3,24 @@ package net.deathware.tut.t10;
 public class Arrays {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        int value = 7;
+        int[] values;
+        values = new int[3];
+        
+        System.out.println(values[0]);
+        
+        values[0]=10;
+        values[1]=20;
+        values[2]=30;
+        for(int i=0;i<values.length;i++){
+            System.out.println(values[i]);
+        }
+        
+        int[] numbers = {5,6,7};
+        
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
 
     }
 
